54db70e8f7cd0ba5e782e643aca5198a01d27e4d
ndathoben <ndathob@gmail.com>
2022-07-04 15:34:03 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * Description: main prints out a string to stdout.
+ * Return: 0 if succcess.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
