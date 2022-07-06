516fcea7c399873e89113702c353b8090bedd211
ndathoben <ndathob@gmail.com>
2022-07-06 16:06:03 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * add - add two intergers and get result
- * Description: it takes two intergers eg 1a and b and adds thus giving results
- * Return : 0
+ * @y: input interger 1
+ * @x: input interger 2
+ * Description: Function adds two ints and returns result
+ * Return : result to add 1 and 2
  */
 int add(int x, int y)
 {
