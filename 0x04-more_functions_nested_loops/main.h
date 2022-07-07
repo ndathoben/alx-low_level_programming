0bfe131390d67dc7032d86397f47037121a2edbd
ndathoben <ndathob@gmail.com>
2022-07-07 13:24:17 -0700

    update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
-void print_numbers(void);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
 void print_triangle(int size);
+void fizz_buzz(void);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+
