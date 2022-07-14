56e9344ef445730a93c498ef97341f39159ad37b
ndathoben <ndathob@gmail.com>
2022-07-14 14:29:11 -0700

    function that compares two strings.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
