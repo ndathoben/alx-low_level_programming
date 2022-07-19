355ce07b2caa9faa57ac30c4e7fdb388e006d7ac
ndathoben <ndathob@gmail.com>
2022-07-19 14:52:32 -0700

    updates

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
