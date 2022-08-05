db757e4d56fe563de2b4144b60b08607ea7fe2ba
ndathoben <ndathob@gmail.com>
2022-08-05 16:34:32 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,8 @@
 #define VARFUN_H
 #include <stdarg.h>
 #include <stdio.h>
-void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
