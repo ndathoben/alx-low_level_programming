3c39b57720b00c4578fdaa62e9f4a7df603e290c
ndathoben <ndathob@gmail.com>
2022-08-19 16:37:57 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,49 +1,48 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x
- * raised to the power of y
- * @x: base number
- * @y: pow number
- * Return: int
+ * _pow - calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
  */
-
-int _pow_recursion(int x, int y)
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= power; i++)
+		num *= base;
+	return (num);
 }
 
 /**
- * print_binary - function that prints the binary representation of a number
- * @n: decimal number
- * Return: nothing
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned int res_pow = 0;
-	int exp = 10;
-	int flag = 0;
+	unsigned long int divisor, check;
+	char flag;
 
-	if (n == 0)
-		_putchar('0');
-	while (exp >= 0)
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
 	{
-		res_pow = _pow_recursion(2, exp);
-		if (n >= res_pow)
+		check = n & divisor;
+		if (check == divisor)
 		{
-			_putchar('1');
-			n -= res_pow;
 			flag = 1;
+			_putchar('1');
 		}
-		else if (n < res_pow && flag == 1)
+		else if (flag == 1 || divisor == 1)
+		{
 			_putchar('0');
-		exp--;
+		}
+		divisor >>= 1;
 	}
 }
